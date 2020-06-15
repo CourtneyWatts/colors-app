85fb92ca8afab0b54439e1e2d3cebeebcbdcebdc
CourtneyWatts <courtney.watts@icloudm>
2020-06-15 16:21:03 +0100

    Palette setup as well as seedPalettes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Palette from './Palette.js'
+import seedPalettes from './seedPalettes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Palette {...seedPalettes[4]} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className='Palette'>
+          {/* Nav */}
+        <div className='Palette-colors'>
+            {/* bunch of colors */}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Palette
