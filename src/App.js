258a30287af90f211d3b035ada03d22688f6cfba
CourtneyWatts <courtney.watts@icloudm>
2020-06-18 11:35:05 +0100

    add ColorHelper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Palette from './Palette.js'
 import seedPalettes from './seedPalettes'
+import { generatePalette } from './ColorHelpers'
 
 function App() {
+  console.log(generatePalette(seedPalettes[4]))
   return (
     <div className='App'>
       <Palette {...seedPalettes[4]} />
